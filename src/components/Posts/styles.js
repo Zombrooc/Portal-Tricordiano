7190aae46ecf3302981f0d05942ab2965c0b6469
Elian Valdez <elian.valdez09@gmail.com>
2022-04-02 23:07:04 -0300

    asaushaushs

--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -1,73 +1,76 @@
 import styled from "styled-components";
 
 export const PostListContainer = styled.section`
-  width: 100%;
-  max-width: 580px;
+  width: 580px;
+  max-width: 100%;
   margin: 0 auto;
   margin-top: 6.257rem;
   padding: 0 30px;
 
-  article {
-    background: #fff;
-    margin-top: 30px;
-  }
-
-  article header {
-    padding: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  article header .user-info {
-    display: flex;
-    flex-direction: column;
-  }
-
-  article header .user-info span {
-    font-size: 13px;
-  }
-
-  article header .user-info span.username {
-    font-size: 11px;
-    color: #666;
-    margin-top: 3px;
-  }
-
-  article > img {
-    width: 100%;
-  }
-
-  article footer {
-    padding: 20px;
-  }
-  /* 
-  article footer .actions {
-    margin-bottom: 10px;
-  } */
-
-  article footer .actions button {
-    background: transparent;
-    border: 0;
-    cursor: pointer;
-    width: auto;
-    display: initial;
+  @media (max-width: 824px) {
     padding: 0;
   }
 
-  article footer .actions svg {
-    height: 19px;
-    margin-right: 12px;
-  }
+  article {
+    background: #fff;
+    margin-top: 13px;
 
-  article footer p {
-    font-size: 13px;
-    margin-top: 2px;
-    line-height: 18px;
-  }
+    & > img,
+    & > video {
+      width: 100%;
+    }
 
-  article footer p span {
-    color: #7159c1;
-    display: block;
+    header {
+      padding: 20px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .user-info {
+        display: flex;
+        flex-direction: column;
+
+        span {
+          font-size: 13px;
+
+          &.username {
+            font-size: 11px;
+            color: #666;
+            margin-top: 3px;
+          }
+        }
+      }
+    }
+
+    footer {
+      padding: 20px;
+
+      .actions {
+        button {
+          background: transparent;
+          border: 0;
+          cursor: pointer;
+          width: auto;
+          display: initial;
+          padding: 0;
+        }
+
+        svg {
+          height: 19px;
+          margin-right: 12px;
+        }
+      }
+
+      p {
+        font-size: 13px;
+        margin-top: 2px;
+        line-height: 18px;
+
+        span {
+          color: #7159c1;
+          display: block;
+        }
+      }
+    }
   }
 `;
