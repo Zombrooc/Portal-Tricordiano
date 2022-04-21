4fdc3db4f2c3f3f5a2b2b7c72414b22fd72f152f
Elian Valdez <elian.valdez09@gmail.com>
2022-04-21 15:50:13 -0300

    aushaushauhas

--- src/components/checkout/CheckoutForm/index.js
+++ src/components/checkout/CheckoutForm/index.js
@@ -36,30 +36,30 @@ const CheckoutForm = ({ options, stripePromise }) => {
       return;
     }
 
-    const clientSecret = new URLSearchParams(window.location.search).get(
-      "payment_intent_client_secret"
-    );
+    // const clientSecret = new URLSearchParams(window.location.search).get(
+    //   "payment_intent_client_secret"
+    // );
 
-    if (!clientSecret) {
-      return;
-    }
+    // if (!clientSecret) {
+    //   return;
+    // }
 
-    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-      switch (paymentIntent.status) {
-        case "succeeded":
-          setMessage("Payment succeeded!");
-          break;
-        case "processing":
-          setMessage("Your payment is processing.");
-          break;
-        case "requires_payment_method":
-          setMessage("Your payment was not successful, please try again.");
-          break;
-        default:
-          setMessage("Something went wrong.");
-          break;
-      }
-    });
+    // stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+    //   switch (paymentIntent.status) {
+    //     case "succeeded":
+    //       setMessage("Payment succeeded!");
+    //       break;
+    //     case "processing":
+    //       setMessage("Your payment is processing.");
+    //       break;
+    //     case "requires_payment_method":
+    //       setMessage("Your payment was not successful, please try again.");
+    //       break;
+    //     default:
+    //       setMessage("Something went wrong.");
+    //       break;
+    //   }
+    // });
   }, [stripe]);
 
   const handleSubmit = async (event) => {
